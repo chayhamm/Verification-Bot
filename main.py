e91865caddcaf57875273ee617e8a6786ca56548
Chay <chayhammond@gmail.com>
2025-03-05 18:40:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,7 @@ async def on_ready():
         print(f"Synced {len(synced)} command(s)")
     except Exception as e:
         print(f"Failed to sync commands: {e}")
+    embed = discord.Embed(title = "Reboot Rust verification", description = "Click the 'Accept' Button to verify now.", color = 0xFF0000)
+    embed.add_field(name = "Why verify?", value = "We, at Reboot, have decided to add this feature to stop bot accounts & scammers from joining the Server.", inline = False)
+    guild = client.get_guild(config["guildID"])
+    channel = guild.get_channel(config["channelID"])
