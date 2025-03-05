5b34e20963a20ddb32d2173e01a3a4ef3a95d2f8
Chay <chayhammond@gmail.com>
2025-03-05 19:03:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from discord import app_commands
 from discord.ext import commands 
 from datetime import datetime
 import json
+import time
 
 with open("config.json") as config:
     config = json.load(config)
 
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix=config["prefix"], intents=intents)
-
 @client.event
 async def on_ready():
     print("bot is ready")
@@ -19,7 +19,29 @@ async def on_ready():
         print(f"Synced {len(synced)} command(s)")
     except Exception as e:
         print(f"Failed to sync commands: {e}")
+    class VerifyButton(discord.ui.View):
+        pass
+        def __init__(
+                self
+            ) -> None:
+            super().__init__()
+            self.value = None
+        @discord.ui.button(label = "Verify", style = discord.ButtonStyle.green)
+        async def verifyButton(self, interaction: discord.Interaction, button: discord.ui.button):
+            verifiedRole = guild.get_role(int(config["verifiedRole"]))
+            user = interaction.user
+            if verifiedRole in user.roles:
+                await interaction.response.send_message("You are already verified!", ephemeral=True)
+            else:
+                await user.add_roles(verifiedRole)
+                await interaction.response.send_message("You have successfully been verified.", ephemeral=True)
     embed = discord.Embed(title = "Reboot Rust verification", description = "Click the 'Accept' Button to verify now.", color = 0xFF0000)
     embed.add_field(name = "Why verify?", value = "We, at Reboot, have decided to add this feature to stop bot accounts & scammers from joining the Server.", inline = False)
-    guild = client.get_guild(config["guildID"])
-    channel = guild.get_channel(config["channelID"])
+    guild = client.get_guild(int(config["guildID"]))
+    channel = guild.get_channel(int(config["channelID"]))
+    message = await channel.send(embed = embed, view = VerifyButton())
+    time.sleep(10)
+    await message.delete()
+    message = await channel.send(embed = embed, view = VerifyButton())
+
+client.run(config["token"])
